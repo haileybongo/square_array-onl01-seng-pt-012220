e7fabad8e466ec606b64481f69c669be89164705
Hailey Bongo <haileyebongo@gmail.com>
2020-01-28 05:00:00 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,9 @@
 def square_array(array)
   array.each do |number|
-  new_number = number ** 2
-  array.replace(new_number)
+    array_new = array
+    new_number = number ** 2
+    array_new << new_number
+    array_new.shift!
+    array = array_new
 end
 end
